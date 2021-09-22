90b8596926e3b7c9609409770aa852cd5a46f631
Evynith <cvega@alumnos.exa.unicen.edu.ar>
2021-09-22 20:45:31 -0300

    se actualizo el test de funcion 'Formatear' acomodandolo a los cambios realizados en las funciones

--- codigo/codigo_test.go
+++ codigo/codigo_test.go
@@ -7,9 +7,24 @@ import (
 )
 
 func TestFormatear(t *testing.T) {
+	var cases = []struct {
+		Input   string // input string in order to be parsed
+		Success bool   // paser result
+		Type    string // the input type
+		Value   string // the input value
+		Length  int    // value length
+	}{
+		{Input: "TX02AB", Success: true, Type: "TX", Value: "AB", Length: 2},
+		{"NN100987654321", true, "NN", "0987654321", 10},
+		{"TX06ABCDE", false, "", "", 0},
+		{"NN04000A", false, "", "", 0},
+	}
 
-	codigo := NuevoCodigo("TX03ABC")
-	resultado := Resultado{"TX", 03, "ABC"}
-	codigoResultante := codigo.Formatear()
-	assert.Equal(t, codigoResultante, resultado)
+	for _, testData := range cases {
+		codigo := NuevoCodigo(testData.Input)
+		_, err := codigo.Formatear()
+		assert.Equal(t, err == nil, testData.Success)
+	}
 }
+
+// go test ./... -count=1
